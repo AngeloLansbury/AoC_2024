ba8b785921b46228ac046a4e40c9d50d54ef1d5c
AngeloLansbury <119609992+AngeloLansbury@users.noreply.github.com>
2024-12-07 10:20:30 -0500

    Create 7b.pl

---
+++ 7b.pl
@@ -0,0 +1,15 @@
+sub recurse {
+  my ($t, $o, @v) = @_;
+  if ($#v > 0) {
+    my $x = shift @v;
+    recurse($t, $o + $x, @v) or recurse($t, $o * $x, @v) or recurse($t, $o . $x, @v);
+  } else {
+    $o + $v[0] == $t or $o * $v[0] == $t or $o . $v[0] == $t;
+  }
+}
+while (<STDIN>) {
+  ($t, $_) = split(/: /, $_);
+  ($o, @v) = split(/ /, $_);
+  $s += $t if recurse($t, $o, @v);
+}
+print $s;
